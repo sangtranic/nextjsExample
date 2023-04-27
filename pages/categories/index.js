4b3ed607a0b9c4ee0b9d1c4eb1db4848ba37b08f
Trần Văn Sáng <sangtranic@gmail.com>
2023-04-27 12:24:43 +0700

    sangtv

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -2,24 +2,58 @@ import React from "react"
 import { withRouter } from "next/router"
 import { LayoutMain } from "@/components/layouts";
 import { getCategory, getMenuMain } from '@/services/main/home';
+import { RadioLink } from '@/components/radioLink';
 import Head from "next/head";
 const CategoryWapper = ({ router, navBarData, dataCategory }) => {
-    
-    const { slug,id } = router.query; 
+
+    const { slug, id } = router.query;
     // const { navBarData } = props;
-    // console.log(navBarData);
     return (
         <LayoutMain navBarData={navBarData}>
             <>
-            <Head>
-                <title>Category Page</title>
-                <meta name="description" content="Example Nexjs" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/vohonline.svg" />
-            </Head>
-            <div className='min-h-screen'>
-            <h1>Category Page { id }</h1>
-            </div>
+                <Head>
+                    <title>Category Page</title>
+                    <meta name="description" content="Example Nexjs" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <link rel="icon" href="/vohonline.svg" />
+                </Head>
+                <div className="container">
+                    <div className="DefaultLayout_content-wrapper__5n_fy">
+                        <div className="Home_home-wrapper__19AOT" >
+                            <div className="Home_body-home-container__1VznX" >
+                                <div className="Home_body-home-left__37mb7">
+                                    {
+                                        dataCategory.map((item, index) => (
+                                            <div key={index} className="NewsList_news-list-item__I2UeH">
+                                                <div className="NewsListItem_news-list-item-thumb__1gs8s">
+                                                    <RadioLink to={item.friendlyUrl}>
+                                                        <img src={item.thumbnail} className="size-image_size-img__1V_wO " alt={item.title} />
+                                                    </RadioLink>
+                                                </div>
+                                                <div className="NewsListItem_news-list-item-wrapper__15Cd1">
+                                                    <div className="NewsListItem_news-list-item-ext__11cfJ">
+                                                        <div className="NewsListItem_time__Jp7h5">
+                                                            <span>{item.publictDay}</span>&nbsp;-&nbsp;
+                                                        </div>
+                                                        <div className="NewsListItem_cate-name__YkB_8">
+                                                            <RadioLink to={item.frenlyCateUrl}>
+                                                                {item.cateName}
+                                                            </RadioLink>
+                                                        </div>
+                                                    </div>
+                                                    <div className="NewsListItem_news-list-item-title__2a-Yt">
+                                                        <RadioLink to={item.friendlyUrl}>{item.title}</RadioLink>
+                                                    </div>
+                                                    <div className="NewsListItem_news-list-item-intro__3Ohhu">{item.intro}</div>
+                                                </div>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </>
         </LayoutMain>
     )
@@ -28,13 +62,12 @@ CategoryWapper.getInitialProps = async ({ query, req }) => {
     const { data: dataMenu } = await getMenuMain(req?.headers['user-agent']);
     const { menu: navBarData } = dataMenu;
     const { data: dataAllFromCategory } = await getCategory(
-      query.id,
-      query.page ? query.page - 1 : 0,
-      req?.headers['user-agent'],
-      query.dow,
+        query.id,
+        query.page ? query.page - 1 : 0,
+        req?.headers['user-agent'],
+        query.dow,
     );
-    const { cateLayoutData: layoutData } = dataAllFromCategory;
-    const { moiCapNhat: dataCategory } = layoutData;
+    const { news: dataCategory } = dataAllFromCategory;
     return { navBarData, dataCategory };
-  };
+};
 export default withRouter(CategoryWapper)
