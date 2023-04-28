5d65b87e810cb9d1b024644150dce09f41c44ccf
Trần Văn Sáng <sangtranic@gmail.com>
2023-04-28 11:31:40 +0700

    sangtv

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ module.exports = routes() // ----   ----      -----
   .add('user', '/user/:id', 'profile') // user   profile   /user/:id
   .add('/:noname/:lang(en|es)/:wow+', 'complex') // (none) complex   /:noname/:lang(en|es)/:wow+
   .add({ name: 'beta', pattern: '/v3', page: 'v3' }) // beta   v3        /v3
+  
   .add('homeRadio', '/radio.html', 'homeRadio')
   .add('radio-category-detail', '/podcast/:slug.:id.html', 'radioCategoryDetail')
   .add('category', '/:slug-:id(\\d+).html', 'categories')
